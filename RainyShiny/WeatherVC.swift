cf553f100e3d35efaf76024ebf1e7b357e353eef
wyodoodoyw <mwood051@gmail.com>
2016-11-02 15:58:10 -0400

    added cocoa pod

---
+++ RainyShiny/WeatherVC.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherVC.swift
+//  RainyShiny
+//
+//  Created by Matthew Wood on 2016-11-02.
+//  Copyright © 2016 Matthew Wood. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+}
+
