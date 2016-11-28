a188dcf0a1254619ff0e9371c4d3a64d9cf43b9e
wyodoodoyw <mwood051@gmail.com>
2016-11-28 12:22:01 -0500

    tableView Delegate Functions

--- RainyShiny/WeatherVC.swift
+++ RainyShiny/WeatherVC.swift
@@ -8,11 +8,42 @@
 
 import UIKit
 
-class WeatherVC: UIViewController {
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // header IB Outlets
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var currentCityLabel: UILabel!
+    @IBOutlet weak var currentWeatherLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    
+    // tableView
+    @IBOutlet weak var tableView: UITableView!
+    
+    // tableView IBOutlets
+    @IBOutlet weak var tvDateLabel: UILabel!
+    @IBOutlet weak var tvHighTemp: UILabel!
+    @IBOutlet weak var tvWeather: UILabel!
+    @IBOutlet weak var tvLowTemp: UILabel!
+    @IBOutlet weak var tvImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
+    }
+    
+    // Table View Delegate Functions
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell
     }
 }
 
